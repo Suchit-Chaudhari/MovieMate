7e3edb3e5a2718248b0c3cf0760ee14985824a45
suchit chaudhari <suchit2409@gmail.com>
2025-02-11 21:43:21 +0530

    today's session

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export var BASE_URL = "https://day-platforms-express-argued.trycloudflare.com"
-export var MOVIE_BASE_URL = "https://eventually-deer-moderate-headlines.trycloudflare.com"
+export var BASE_URL = "https://e666-43-243-83-230.ngrok-free.app"
+export var MOVIE_BASE_URL = "https://rj-copyright-catalogs-pa.trycloudflare.com"
