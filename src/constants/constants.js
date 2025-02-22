f7ff6d900a0e68fd68baf22d7ee624ca0979f385
suchit chaudhari <suchit2409@gmail.com>
2025-02-22 14:49:44 +0530

    today

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export var BASE_URL = "https://clothes-scene-stupid-yrs.trycloudflare.com";
-export var MOVIE_BASE_URL = "https://projectors-retailers-judge-messenger.trycloudflare.com"
+export var BASE_URL = "https://arising-avoid-producers-continuously.trycloudflare.com";
+export var MOVIE_BASE_URL = "https://wet-found-shade-reading.trycloudflare.com"
 // http://192.168.1.24:7031
