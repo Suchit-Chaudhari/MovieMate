c4835dd45580279f69e3a2618f2ca16944e53e53
suchit chaudhari <suchit2409@gmail.com>
2025-02-22 21:39:19 +0530

    sfhasd

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export var BASE_URL = "https://arising-avoid-producers-continuously.trycloudflare.com";
-export var MOVIE_BASE_URL = "https://wet-found-shade-reading.trycloudflare.com"
+export var BASE_URL = "https://closure-champions-anti-bottom.trycloudflare.com";
+export var MOVIE_BASE_URL = "https://measurement-frontpage-polo-socket.trycloudflare.com"
 // http://192.168.1.24:7031
