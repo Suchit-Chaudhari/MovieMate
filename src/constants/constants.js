0e7629b9a33b55d4edee55d98c426f9474f9b9d5
suchit chaudhari <suchit2409@gmail.com>
2025-03-01 16:07:30 +0530

    movie url update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export var BASE_URL = "https://950d-43-243-83-232.ngrok-free.app";
-export var MOVIE_BASE_URL = "https://d270-43-243-83-232.ngrok-free.app";
+export var MOVIE_BASE_URL = "https://8082-43-243-83-232.ngrok-free.app";
 // http://192.168.1.24:7031
