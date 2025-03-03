1aaa93f7683bf49e463125040a4c38dee922750f
suchit chaudhari <suchit2409@gmail.com>
2025-03-03 15:45:53 +0530

    backend is live

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export var BASE_URL = "https://950d-43-243-83-232.ngrok-free.app";
-export var MOVIE_BASE_URL = "https://8082-43-243-83-232.ngrok-free.app";
+export var BASE_URL = "https://moviemate-backend3.onrender.com";
+export var MOVIE_BASE_URL = "https://7bd8-43-243-83-232.ngrok-free.app";
 // http://192.168.1.24:7031
