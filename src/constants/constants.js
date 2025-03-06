09d29f140c5734d61563dfb054418d2c94366448
suchit chaudhari <161592195+Suchit-Chaudhari@users.noreply.github.com>
2025-03-06 16:20:10 +0530

    Update constants.js solved movie file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export var BASE_URL = "https://moviemate-backend3.onrender.com";
-export var MOVIE_BASE_URL = "https://4a08-114-143-138-98.ngrok-free.app/movies/sources/sanam.mp4";
+export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/fixed-video.mp4?sp=r&st=2025-03-06T10:49:17Z&se=2025-03-06T18:49:17Z&spr=https&sv=2022-11-02&sr=b&sig=tJEpwWZBNx0hLLjWI%2BzeDuhELDbwEK9pV2uWJI5sf7M%3D";
 // http://192.168.1.24:7031
