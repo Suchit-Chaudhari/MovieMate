88c0103a29e6ee799b5798d144f4ffdc8497b7c3
suchit chaudhari <161592195+Suchit-Chaudhari@users.noreply.github.com>
2025-03-10 21:37:29 +0530

    Update constants.js this should be working

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export var BASE_URL = "https://moviemate-backend3.onrender.com";
-export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/Rx100.mp4";
+export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/RX%20100%20_%20Kartikeya%20and%20Payal%20Rajput%20_%20New%20Released%20South%20Indian%20Action%20Movie%20Dubbed%20In%20Hindi%20Full.mp4?sp=r&st=2025-03-10T16:06:24Z&se=2025-04-30T00:06:24Z&spr=https&sv=2022-11-02&sr=b&sig=4bYe78VWqhtBUK2yPcd740vt29potM7cF0m%2FW%2BXWKoc%3D";
 // http://192.168.1.24:7031
