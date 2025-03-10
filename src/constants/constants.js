cfdcb6fe377cba50f8cabf2b05ce0e684e93310c
suchit chaudhari <161592195+Suchit-Chaudhari@users.noreply.github.com>
2025-03-10 16:49:41 +0530

    Update constants.js checking normal URL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export var BASE_URL = "https://moviemate-backend3.onrender.com";
-export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/Rx100.mp4?sp=r&st=2025-03-10T11:09:59Z&se=2025-04-29T19:09:59Z&spr=https&sv=2022-11-02&sr=b&sig=wOedvp8XDhOjfLB0j3ppgNVmUr1wzxlV9Z2zKBvF%2F9U%3D";
+export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/Rx100.mp4";
 // http://192.168.1.24:7031
