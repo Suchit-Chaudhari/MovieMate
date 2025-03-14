73936fc7ec58ed72134f26d7c3ff6bbad5e8b3ec
suchit chaudhari <161592195+Suchit-Chaudhari@users.noreply.github.com>
2025-03-14 12:18:12 +0530

    Update constants.js tholi prema

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export var BASE_URL = "https://moviemate-backend3.onrender.com";
-export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/RX%20100%20_%20Kartikeya%20and%20Payal%20Rajput%20_%20New%20Released%20South%20Indian%20Action%20Movie%20Dubbed%20In%20Hindi%20Full.mp4?sp=r&st=2025-03-10T16:06:24Z&se=2025-04-30T00:06:24Z&spr=https&sv=2022-11-02&sr=b&sig=4bYe78VWqhtBUK2yPcd740vt29potM7cF0m%2FW%2BXWKoc%3D";
+export var MOVIE_BASE_URL = "https://movieblob.blob.core.windows.net/movies/Tholi%20Prema%20(HD)%20%20New%20Romantic%20Hindi%20Dubbed%20Full%20Movie%20%20Varun%20Tej%2C%20Raashi%20Khanna.mp4?sp=r&st=2025-03-14T06:46:15Z&se=2025-04-30T14:46:15Z&spr=https&sv=2022-11-02&sr=b&sig=gkfYlmUEa5v%2BIPT%2BB1wm48lNn4rmurAZ8FSrcj9AQkw%3D";
 // http://192.168.1.24:7031
